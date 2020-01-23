78ef7e1b05e3e532152552a6c8d5d80ad09638e0
Mahesh <tmp.mahesh@gmail.com>
2020-01-24 03:14:54 +0700

    Update Boss Banteng
    Sisa melee attack triggernya

---
+++ Pentagone/Assets/Scripts/Boss/Boss Banteng/Boss2Sound.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss2Sound : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip charge;
+    [SerializeField] private AudioClip steady;
+    [SerializeField] private AudioClip breath;
+    [SerializeField] private AudioClip crash;
+
+    [Space(10)]
+    [Header("Bool")]
+
+    [SerializeField] private bool isCharging;
+
+    private void Awake()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ChargeSound() {
+        if (!isCharging)
+        {
+            isCharging = true;
+            //audioSource.PlayOneShot(charge);
+        }
+    }
+
+    public void CrashSound() 
+    {
+        isCharging = false;
+    }
+
+    public void HitSound()
+    {
+        isCharging = false;
+    }
+
+    public void BreathSound() {
+        audioSource.PlayOneShot(breath);
+    }
+
+    public void SteadySound()
+    {
+        audioSource.PlayOneShot(steady);
+    }
+}
